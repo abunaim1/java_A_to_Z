0714810cf12daca6598ce7c36b05df68203aaf05
Abu Naim <aabunnaim@gmail.com>
2025-12-20 01:42:53 +0600

    Starting java code

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args){
+        // This is my first java program
+        /*
+            This
+            is
+            a multi line
+            comment
+         */
+        System.out.println("I like Burger!");
+        System.out.print("It's really good!\n");
+        System.out.println("Bu me Burger!");
+    }
+}
