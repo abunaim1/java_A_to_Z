9561c4550ab22753d30e767dfb62b676b82405d0
Abu Naim <aabunnaim@gmail.com>
2026-01-25 16:44:56 +0600

    Class and object

---
+++ JavaOOP/Car.java
@@ -0,0 +1,16 @@
+package JavaOOP;
+
+public class Car {
+    String model="Audi";
+    int age;
+    int price;
+    boolean isSold;
+
+    Car(String model, int age, int price){
+        this.model = model;
+        this.age = age;
+        this.price = price;
+        this.isSold = true;
+    }
+    
+}
---
+++ JavaOOP/Main.java
@@ -0,0 +1,9 @@
+package JavaOOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("Audi", 2, 22);
+        System.out.println(car.isSold);
+    }
+}
